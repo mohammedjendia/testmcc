8287100b9a03ac282ac828b2a74f5b5d96ade61b
mohammedjendia <mhmed98614010@gmail.com>
2021-03-17 20:13:33 +0200

    add welcome on create

--- app/src/main/java/com/example/testmcc/MainActivity.kt
+++ app/src/main/java/com/example/testmcc/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.testmcc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("TAG","onCreate: Welcom");
     }
 }
